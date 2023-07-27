9587191d348f001bc0bcc9ea503da40a39909ca3
BrianRO21 <bro212311@gmail.com>
2023-07-26 20:43:19 -0600

    Semana10

--- Proyecto JS/js/listarcurso.js
+++ Proyecto JS/js/listarcurso.js
@@ -1,5 +1,9 @@
 var apibase = "https://paginas-web-cr.com/ApiPHP/apis/";
 var apiconsultar = "ListaCurso.php";
+var apieliminar = "BorrarCursos.php";
+
+const myModalEliminar = new bootstrap.Modal(document.getElementById('myModalEliminar'));
+
 
 let tablaresultado = document.querySelector('#tablaresultado');
 
@@ -20,29 +24,54 @@ function consultardatos(){
 function ajustardatostabla(datos){
     console.log("datos"+datos);
     for (const objetoindividual of datos) {
-    //    console.log(objetoindividual.id);
-    //    console.log(objetoindividual.nombre);
-    //    console.log(objetoindividual.descripcion);
-    //    console.log(objetoindividual.tiempo);
-    //    console.log(objetoindividual.usuario);
-    //    console.log("///////////");
-
-        tablaresultado.innerHTML += `
-            <tr class="table-primary" >
+       tablaresultado.innerHTML += `
+            <tr class="table-primary">
                                 <td scope="row">${objetoindividual.id}</td>
                                 <td>${objetoindividual.nombre}</td>
                                 <td>${objetoindividual.descripcion}</td>
                                 <td>${objetoindividual.tiempo}</td>
                                 <td>${objetoindividual.usuario}</td>
-                                <td>---</td>                              
+                                <td>
+                                    <a name="Eliminar" id="Eliminar" class="btn btn-danger" role="button" onclick="mostrarModal('${objetoindividual.id}')">Eliminar</a>
+                                </td>                              
             </tr>
        `;
-    }
-
-
-    // {"id":"3086","nombre":"Api Jul 19","descripcion":"Expres","descripcion":"85","usuario":"Kevin M. VLA"}
-           
+    }   
 }
 
+
+    function mostrarModal(id){
+
+        eliminandodato(id);
+
+        myModalEliminar.show();
+        
+    }
+
+    function eliminandodato(id){
+
+        var datosEnviar = { 
+            "id":id 
+        }
+
+        apiurl = apibase + apieliminar ;
+        fetch(apiurl,
+            {
+                method:'POST',
+                body: JSON.stringify(datosEnviar)
+            })
+        .then(estructura => estructura.json())
+        .then((datosrespuesta) => {
+                completeDelete()
+            })
+        .catch(console.log);
+    }
+
+    function completeDelete(){
+        myModalEliminar.hide();
+        tablaresultado.innerHTML = ``;
+        consultardatos();
+        
+    }
 
 consultardatos();
