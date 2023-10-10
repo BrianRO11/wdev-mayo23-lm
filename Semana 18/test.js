08e31cb3bfc2012218407ac3337514aba8514407
BrianRO21 <bro212311@gmail.com>
2023-10-09 19:30:07 -0600

    Node js

---
+++ Semana 18/test.js
@@ -0,0 +1,24 @@
+const titulo = "Semana 18"
+console.log(titulo);
+console.log(typeof titulo);
+
+const numerocualquiera = 10;
+console.log(numerocualquiera);
+console.log(typeof numerocualquiera);
+
+const bolean = true;
+console.log(bolean);
+console.log(typeof bolean);
+
+//Tipos de lenguaje 
+//Estaticos
+// c++
+// java
+// c#
+//Por ejemplo 
+//number variable = 0;
+
+//Dinamicos no necesitan saber cual es el tipo de dato
+// js
+// pyton
+// var variable = 0;
