ede3aac46e3d3225a891b6334f1277e37a830a1f
BrianRO21 <bro212311@gmail.com>
2023-10-11 20:52:12 -0600

    Angular Intro

---
+++ Semana 18/miercoles/dist/ejemplo.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("Semana 18 Typescript");
+let nombre = "clase miercoles";
+nombre = "90";
+let text = "Brian";
+let number = 10000;
+let booleano = true;
+function test(nombre) {
+    return "hola" + nombre;
+}
+let carros = ['bmw', 'nissan', 'toyota', "12"];
---
+++ Semana 18/miercoles/src/ejemplo.ts
@@ -0,0 +1,22 @@
+//Compilation
+
+console.log("Semana 18 Typescript")
+
+let nombre: string = "clase miercoles";
+
+nombre = "90";
+
+
+//Tipos de datos 
+let text: string = "Brian";
+let number: number = 10000;
+let booleano: boolean = true;
+
+//test(numero);
+
+function test(nombre: string){
+    return "hola"+nombre;
+}
+
+let carros: string [] = ['bmw', 'nissan', 'toyota', "12"];
+
