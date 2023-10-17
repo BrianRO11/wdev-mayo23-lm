57fec2dc7394fa5b1fcc34f98cb80e588646e093
BrianRO21 <bro212311@gmail.com>
2023-10-16 19:50:19 -0600

    Angular

--- Proyecto Angular/primerAngular/src/app/app.component.ts
+++ Proyecto Angular/primerAngular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Cursos} from './models/cursos';
+//import Importa librerias o clases
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //Atributos
   title = 'primerAngular';
+
+  arregloDatos: Cursos [] = [
+    {id: "1", nombre: "html5", descripcion: "111", tiempo: "11", usuario: "Brian"},
+    {id: "2", nombre: "css3", descripcion: "222", tiempo: "22", usuario: "Brian"},
+    {id: "3", nombre: "js", descripcion: "333", tiempo: "33", usuario: "Brian"},
+    {id: "4", nombre: "jquery", descripcion: "444", tiempo: "44", usuario: "Brian"},
+    {id: "5", nombre: "bootstrap", descripcion: "555", tiempo: "55", usuario: "Brian"}
+  ]
 }
+
+
+// id: "", nombre: "", descripcion: "", tiempo: "", usuario: ""
