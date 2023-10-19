43b51ba5afcc1cd877c534dffd47661ea004fa5c
BrianRO21 <bro212311@gmail.com>
2023-10-18 19:29:28 -0600

    Angular

--- Proyecto Angular/primerAngular/src/app/app.component.ts
+++ Proyecto Angular/primerAngular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Cursos} from './models/cursos';
+import { RespuestaCursos } from './models/respuesta-cursos';
 //import Importa librerias o clases
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
@@ -31,10 +32,10 @@ export class AppComponent implements OnInit {
   obtenerDatos(){
     const url = "https://paginas-web-cr.com/ApiPHP/apis/ListaCurso.php";
 
-    this.http.get<Cursos[]>(url).subscribe(
+    this.http.get<RespuestaCursos>(url).subscribe(
       (response) => {
-      //this.ArregloDatos = response.data;
-      console.log(response);
+      this.ArregloDatos = response.data;
+      console.log(response.data);
     },
     (error) =>{
       console.error("Error en la carga de datos", error)
--- Proyecto Angular/primerAngular/src/app/models/cursos.ts
+++ Proyecto Angular/primerAngular/src/app/models/cursos.ts
@@ -6,3 +6,8 @@ export class Cursos {
     usuario: string= "";
 }
 
+// export interface CursoResponse {
+//     code: number;
+//     message: string;
+//     data: Cursos[];
+//   }
