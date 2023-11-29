c93e424ddfd610e3e145612cad8e7d4103eae386
BrianRO21 <bro212311@gmail.com>
2023-11-29 17:35:55 -0600

    Inicio React

--- Proyecto React/academia/src/App.js
+++ Proyecto React/academia/src/App.js
@@ -1,27 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap'
+
+//Seccion de Componentes
+import ListarCurso from './componentsCurso/index';
 
 function App() {
   return (
-<div class="container-fluid">
-  <ul class="nav nav-tabs" id="myTab" role="tablist">
-    <li class="nav-item" role="presentation">
-      <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
-    </li>
-    <li class="nav-item" role="presentation">
-      <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
-    </li>
-    <li class="nav-item" role="presentation">
-      <button class="nav-link" id="messages-tab" data-bs-toggle="tab" data-bs-target="#messages" type="button" role="tab" aria-controls="messages" aria-selected="false">Messages</button>
-    </li>
-  </ul>
-  
-  <div class="tab-content">
-    <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab"> Casa </div>
-    <div class="tab-pane" id="profile" role="tabpanel" aria-labelledby="profile-tab"> profile </div>
-    <div class="tab-pane" id="messages" role="tabpanel" aria-labelledby="messages-tab"> messages </div>
-  </div>
+
+  <div className="container">
+    <ListarCurso></ListarCurso>
 </div>
+
   );
 }
 
