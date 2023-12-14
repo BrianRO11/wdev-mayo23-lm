0a61d9ea53a3db6a2f2bce8bf2bbe37a859926ab
BrianRO21 <bro212311@gmail.com>
2023-12-13 18:41:05 -0600

    Vue initiation

---
+++ ProyectoVue/vla-frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <FormularioBienvenida></FormularioBienvenida>
+  <SaludarAlPublico 
+  nombrePersona='Brian Rivas' 
+  lugarClase="Frontend Developer" 
+  horarioClase="Lunes Noche"></SaludarAlPublico>
+  <SaludarAlPublico 
+  nombrePersona='German Ariza' 
+  lugarClase="Python" 
+  horarioClase="Miercoles Noche"></SaludarAlPublico>
+</template>
+
+<script>
+
+import FormularioBienvenida from './components/FormularioBienvenida.vue';
+import SaludarAlPublico from './components/SaludarAlPublico.vue';
+
+export default {
+  name: 'App',
+  components: 
+  {
+    FormularioBienvenida,
+    SaludarAlPublico
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ProyectoVue/vla-frontend/src/components/FormularioBienvenida.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <h1>Solicitando datos</h1>
+        <p>Digite su nombre</p>
+        <input class="form-control" id="textoNombre">
+        <br>
+        <button id="botonNombre" class="btn btn-primary"
+            @click="VerNombre">Mostrar nombre</button>
+        <br>
+        <h3></h3>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: 
+    {
+        VerNombre()
+        {
+            var nombre = document.getElementById("textoNombre").value;
+            alert("El nombre es: " + nombre)
+        }
+    }
+}
+
+</script>
